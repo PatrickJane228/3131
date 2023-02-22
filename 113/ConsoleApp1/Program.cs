c16d5efbd293ee0141b8a2318afcd683b829a2ac
Ilya <123509915+PatrickJane228@users.noreply.github.com>
2023-02-22 11:09:52 +0200

    Add files via upload

---
+++ 113/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            byte n = byte.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+
+            {//Цикл для заполнения с помощью метода SetValue
+                Console.Write($"array [{i}] =");
+                array.SetValue(int.Parse(Console.ReadLine()), i);
+            }
+            //Цикл вывода элементов ОМ( одномерный массив)   
+            // for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine($"{array.GetValue(i)}");
+            //}
+            //Console.WriteLine();
+            //Array.Reverse(array);
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine($"{array.GetValue(i)}");
+            //}
+            ////Изменение порядка расположения элементов ОМ в обратном порядке.
+            //Console.ReadKey();
+
+            //Обнуление элементов в ОМ
+            Console.WriteLine();
+            Array.Reverse(array);
+
+
+
+        }
+    }
+}
